68e2feca15c9a418dcebe577510eea0a7f26edf7
dhammar <dhammar@collegenet.com>
2018-12-01 14:37:27 -0800

    cleanup

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,9 +4,9 @@ using TMPro;
 public class ScoreManager : MonoBehaviour {
 
     public int score;
-    private TextMeshProUGUI textMeshProUGUI;
 	// Update is called once per frame
 	public void Update() {
         GetComponent<TextMeshProUGUI>().SetText(score.ToString());
 	}
 }
+ 
