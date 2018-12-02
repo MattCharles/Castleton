9b132a7b95f3ebfe5b8c05b4fd3c9aebf1e18fe7
mattcharles <matthew.c.anthony.miller@gmail.com>
2018-12-02 15:07:19 -0800

    Computer now builds a shit base at game start

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -5,14 +5,22 @@ public class Computer : MonoBehaviour, IActor
     public Inventory inventory;
     public ActorState state;
     public GameObject MyPlatform;
-    public Vector3 TargetPlatform;
-    public float centerToSpawnEdge = 5f;
+    public GameObject TargetPlatform;
+    public float ySpawnVariance = 3f;
+    public float centerToSpawnEdge = 1f;
     public Cannon Cannon;
     public ActorType type = ActorType.computer;
+    Vector3 PlatformCenter;
 
     public new ActorType GetType()
     {
         return type;
+    }
+
+    void Awake()
+    {
+        PlatformCenter = new Vector3(-10f, 1f, 0f);
+        PlaceBlock();
     }
 
     public void PlaceBlock()
@@ -20,20 +28,26 @@ public class Computer : MonoBehaviour, IActor
         state = ActorState.Placing;
         foreach(Block block in inventory.blocks)
         {
-            if(Random.Range(0, 1) > .5f) {
+            if(Random.Range(0f, 1f) > .35f) {
                 block.state = Block.BlockState.ShootingBlock;
                 continue;
             }
-            Instantiate(block, GetRandomPlacement(), Quaternion.identity);
+            // Block copy = Instantiate(block, GetRandomPlacement(), Quaternion.identity);
+            block.transform.position = GetRandomPlacement();
+            Debug.Log(block.transform.position);
+            block.gameObject.SetActive(true);
             block.state = Block.BlockState.Placed;
         }
     }
 
     private Vector3 GetRandomPlacement()
     {
-        float x = MyPlatform.transform.position.x + UnityEngine.Random.Range(-centerToSpawnEdge, centerToSpawnEdge);
-        float y = MyPlatform.transform.position.y + 1;
-        float z = MyPlatform.transform.position.z + UnityEngine.Random.Range(-centerToSpawnEdge, centerToSpawnEdge);
+        float r1 = UnityEngine.Random.Range(-centerToSpawnEdge, centerToSpawnEdge);
+        float r2 = UnityEngine.Random.Range(-centerToSpawnEdge, centerToSpawnEdge);
+        float deltaY = UnityEngine.Random.Range(0f, ySpawnVariance);
+        float x = PlatformCenter.x + r1;
+        float y = PlatformCenter.y + deltaY;
+        float z = PlatformCenter.z + r2;
         return new Vector3(x, y, z);
     }
 
@@ -47,7 +61,7 @@ public class Computer : MonoBehaviour, IActor
         state = ActorState.Shooting;
         foreach(Block block in inventory.blocks)
         {
-            Cannon.AIShoots(block, TargetPlatform);
+            Cannon.AIShoots(block, TargetPlatform.transform.position);
         }
     }
 
